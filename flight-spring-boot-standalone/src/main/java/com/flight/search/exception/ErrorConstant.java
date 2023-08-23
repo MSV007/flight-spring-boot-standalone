b9352285c4f86752feb1296d72f2852adcbf8ffa
MSV007 <68530973+MSV007@users.noreply.github.com>
2023-08-23 16:00:06 +0530

    Update ErrorConstant.java

--- flight-spring-boot-standalone/src/main/java/com/flight/search/exception/ErrorConstant.java
+++ flight-spring-boot-standalone/src/main/java/com/flight/search/exception/ErrorConstant.java
@@ -4,7 +4,7 @@ public class ErrorConstant {
 
     public static String TIMESTAMP = "timestamp";
     public static String ERROR_MESSAGE = "errorMessage";
-    public static String ERROR = "timestamp";
+    public static String ERROR = "error";
     public static String GENERIC_EXCEPTION_MESSAGE = "An error occurred while processing request :";
     public static String FILE_NOT_FOUND_EXCEPTION_MESSAGE = "Flights are not available for this route :";
     public static String SQL_EXCEPTION_MESSAGE = "An SQL error occurred :";
